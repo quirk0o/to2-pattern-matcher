cda77441e700c2c85282034356abe0b5fcb2e0fc
Quirk0o <beata@obrok.eu>
2015-12-02 13:00:39 +0100

    Add getView function to PatternMatcher interface

--- src/pl/edu/agh/to2/frazeusz/pattern_matcher/IPatternMatcher.java
+++ src/pl/edu/agh/to2/frazeusz/pattern_matcher/IPatternMatcher.java
@@ -3,14 +3,18 @@ package pl.edu.agh.to2.frazeusz.pattern_matcher;
 import pl.edu.agh.to2.frazeusz.models.SearchPattern;
 import pl.edu.agh.to2.frazeusz.nlprocessor.IWordProvider;
 
+import javax.swing.*;
 import java.util.List;
 
 public interface IPatternMatcher extends IMatchProvider {
+
     List<String> match(List<String> sentences, String url);
 
     void addPattern(SearchPattern pattern);
     void removePattern(SearchPattern pattern);
     List<SearchPattern> getPatterns();
+
+    JPanel getView();
 
     void setWordProvider(IWordProvider wordProvider);
 }
