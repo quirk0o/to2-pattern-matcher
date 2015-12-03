78b82cacb42ded6d14779fe9da737379d38884f9
Quirk0o <beata@obrok.eu>
2015-12-03 03:23:21 +0100

    Implement ListMatchingStrategy

--- src/pl/edu/agh/to2/frazeusz/pattern_matcher/matcher/regex/ListMatchingStrategy.java
+++ src/pl/edu/agh/to2/frazeusz/pattern_matcher/matcher/regex/ListMatchingStrategy.java
@@ -2,25 +2,61 @@ package pl.edu.agh.to2.frazeusz.pattern_matcher.matcher.regex;
 
 import pl.edu.agh.to2.frazeusz.nlprocessor.IWordProvider;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class ListMatchingStrategy extends AbstractMatchingStrategy {
 
     private List<IMatchingStrategy> strategies = new LinkedList<>();
 
-    public ListMatchingStrategy(IWordProvider wordProvider) {
+    public ListMatchingStrategy(List<IMatchingStrategy> strategies, IWordProvider wordProvider) {
         super(wordProvider);
+        this.strategies = strategies;
     }
 
     @Override
     public String format(String pattern) {
-        return null;
+        Map<String, Set<String>> wordMap = getWords(pattern);
+
+        for (String word : wordMap.keySet())
+            pattern = pattern.replaceAll(word, join(wordMap.get(word)));
+
+        return pattern;
     }
 
     @Override
     public Pattern compile(String pattern) {
-        return null;
+        return Pattern.compile(pattern, getFlags());
+    }
+
+    @Override
+    public Map<String, Set<String>> getWords(String pattern) {
+        Map<String, Set<String>> wordMap = new HashMap<>();
+        Map<String, Set<String>> tmpMap;
+        Set<String> addWords;
+
+        for (IMatchingStrategy strategy : strategies) {
+            tmpMap = strategy.getWords(pattern);
+            for (String word : tmpMap.keySet()) {
+                addWords = tmpMap.get(word);
+
+                if (wordMap.containsKey(word))
+                    wordMap.get(word).addAll(addWords);
+                else
+                    wordMap.put(word, addWords);
+            }
+        }
+
+        return wordMap;
+    }
+
+    @Override
+    public int getFlags() {
+        int flags = 0;
+
+        for (IMatchingStrategy strategy : strategies)
+            flags |= strategy.getFlags();
+
+        return flags;
     }
 }
