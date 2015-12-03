1cfcc6fe0056d532885f1e378230484c80081667
Quirk0o <beata@obrok.eu>
2015-12-03 03:00:29 +0100

    Implement getWords for DiminutiveStrategy

--- src/pl/edu/agh/to2/frazeusz/pattern_matcher/matcher/regex/DiminutiveStrategy.java
+++ src/pl/edu/agh/to2/frazeusz/pattern_matcher/matcher/regex/DiminutiveStrategy.java
@@ -2,6 +2,7 @@ package pl.edu.agh.to2.frazeusz.pattern_matcher.matcher.regex;
 
 import pl.edu.agh.to2.frazeusz.nlprocessor.IWordProvider;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class DiminutiveStrategy extends AbstractMatchingStrategy {
@@ -22,5 +23,18 @@ public class DiminutiveStrategy extends AbstractMatchingStrategy {
         }
 
         return pattern;
+    }
+
+    @Override
+    public Set<String> getWords(String pattern) {
+        Set<String> words = split(pattern);
+        Set<String> allWords = new HashSet<>();
+
+        for (String word : words) {
+            allWords.add(word);
+            allWords.addAll(wordProvider.getDiminutives(word));
+        }
+
+        return allWords;
     }
 }
