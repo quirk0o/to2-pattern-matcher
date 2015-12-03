bf65b18246761d37ca29857d4e81b0a59b0bd03b
Quirk0o <beata@obrok.eu>
2015-12-03 03:01:10 +0100

    Fix for ListMatchinStrategy implementing interface instead of extending abstract class

--- src/pl/edu/agh/to2/frazeusz/pattern_matcher/matcher/regex/ListMatchingStrategy.java
+++ src/pl/edu/agh/to2/frazeusz/pattern_matcher/matcher/regex/ListMatchingStrategy.java
@@ -1,12 +1,18 @@
 package pl.edu.agh.to2.frazeusz.pattern_matcher.matcher.regex;
 
+import pl.edu.agh.to2.frazeusz.nlprocessor.IWordProvider;
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-public class ListMatchingStrategy implements IMatchingStrategy {
+public class ListMatchingStrategy extends AbstractMatchingStrategy {
 
     private List<IMatchingStrategy> strategies = new LinkedList<>();
+
+    public ListMatchingStrategy(IWordProvider wordProvider) {
+        super(wordProvider);
+    }
 
     @Override
     public String format(String pattern) {
