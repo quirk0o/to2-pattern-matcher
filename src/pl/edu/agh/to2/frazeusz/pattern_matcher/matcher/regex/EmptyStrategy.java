5ae5d149fd485b27aad883f3ac59e1d7590c13ac
Quirk0o <beata@obrok.eu>
2015-12-03 07:12:17 +0100

    Remove unused import

--- src/pl/edu/agh/to2/frazeusz/pattern_matcher/matcher/regex/EmptyStrategy.java
+++ src/pl/edu/agh/to2/frazeusz/pattern_matcher/matcher/regex/EmptyStrategy.java
@@ -2,8 +2,6 @@ package pl.edu.agh.to2.frazeusz.pattern_matcher.matcher.regex;
 
 import pl.edu.agh.to2.frazeusz.nlprocessor.IWordProvider;
 
-import java.util.regex.Pattern;
-
 public class EmptyStrategy extends AbstractMatchingStrategy {
 
     public EmptyStrategy(IWordProvider wordProvider) {
