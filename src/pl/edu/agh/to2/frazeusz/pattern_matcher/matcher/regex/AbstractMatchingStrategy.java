cb9aa55d2737fe0674ee9745bffedaaebbbc6c0b
Quirk0o <beata@obrok.eu>
2015-12-03 02:55:50 +0100

    Add utility functions to AbstractMatchingStrategy for splitting pattern into words and joining words into pattern

--- src/pl/edu/agh/to2/frazeusz/pattern_matcher/matcher/regex/AbstractMatchingStrategy.java
+++ src/pl/edu/agh/to2/frazeusz/pattern_matcher/matcher/regex/AbstractMatchingStrategy.java
@@ -3,15 +3,33 @@ package pl.edu.agh.to2.frazeusz.pattern_matcher.matcher.regex;
 import pl.edu.agh.to2.frazeusz.nlprocessor.IWordProvider;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public abstract class AbstractMatchingStrategy implements IMatchingStrategy {
+
+    protected Pattern wordPattern = Pattern.compile("(?<=\\s|^)[^\\s*]+(?=\\s|$)");
 
     protected IWordProvider wordProvider;
 
     public AbstractMatchingStrategy(IWordProvider wordProvider) {
         this.wordProvider = wordProvider;
+    }
+
+    protected Set<String> split(String pattern) {
+        Set<String> matches = new HashSet<>();
+        Matcher matcher = wordPattern.matcher(pattern);
+        while (matcher.find())
+            matches.add(matcher.group());
+
+        return matches;
+    }
+
+    protected String join(Iterable<String> words) {
+        return String.format("(%s)", String.join("|", words));
     }
 
     @Override
